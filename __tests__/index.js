f966a4bd991b3bb27c0056a49ba8833038dc17b3
Stephen Edgar <stephen@netweb.com.au>
2016-01-26 10:47:28 +1100

    Remove unneeded catches

--- __tests__/index.js
+++ __tests__/index.js
@@ -73,9 +73,6 @@ test("no warnings with valid css", t => {
     t.notOk(errored, "no errored")
     t.is(warnings.length, 0, "flags no warnings")
   })
-  .catch(function (err) {
-    console.error(err.stack)
-  })
 })
 
 test("a warning with invalid css", t => {
@@ -89,8 +86,5 @@ test("a warning with invalid css", t => {
     t.ok(errored, "errored")
     t.is(warnings.length, 1, "flags one warning")
     t.is(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
-  })
-  .catch(function (err) {
-    console.error(err.stack)
   })
 })
